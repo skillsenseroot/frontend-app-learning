48f6e674c2a01dd8c73f4680ce61a0ef18e65351
Robin Kumar <38507037+robinkumar99@users.noreply.github.com>
2024-06-14 02:22:54 +0530

    initial commit

--- env.config.jsx
+++ env.config.jsx
@@ -1,23 +1,9 @@
-import UnitTranslationPlugin from '@edx/unit-translation-selector-plugin';
 import { PLUGIN_OPERATIONS, DIRECT_PLUGIN } from '@openedx/frontend-plugin-framework';
 
 // Load environment variables from .env file
 const config = {
   ...process.env,
   pluginSlots: {
-    unit_title_plugin: {
-      plugins: [
-        {
-          op: PLUGIN_OPERATIONS.Insert,
-          widget: {
-            id: 'unit_title_plugin',
-            type: DIRECT_PLUGIN,
-            priority: 1,
-            RenderWidget: UnitTranslationPlugin,
-          },
-        },
-      ],
-    },
     footer_slot: {
       plugins: [
         {
